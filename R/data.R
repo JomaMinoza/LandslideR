7ba8218f9f316680f93eef10c0dca3d3ceebef6a
Jose Marie Antonio Minoza <josemarieantoniominoza@gmail.com>
2022-04-20 05:41:37 +0800

    Add description for datasets

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Northwest Borneo Datasets
+#'
+#' These datasets contain the area of concern where there could be
+#' submarine landslide in the tip the Northwest Borneo and the
+#' assumed scarp area
+#'
+#' @name nwbt.slide.area
+#' @keywords datasets
+NULL
+
+#' @rdname nwbt.slide.area
+"nwbt.slide.area"
+
+#' @rdname nwbt.slide.area
+"nwbt.slide.area.scarp"
